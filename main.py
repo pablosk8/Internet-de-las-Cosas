84a4fae90b506866d335830457858fde32067d46
Pablo F <50145985+pablosk8@users.noreply.github.com>
2023-07-10 17:49:48 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# Tarea cap 2
+from machine import Pin
+import time
+
+print("esperando pulsador")
+
+sw = Pin(23, Pin.IN)
+led = Pin(2, Pin.OUT)
+
+contador=0
+while True:
+    if sw.value():
+        contador += 1
+        if contador == 10:
+           led.value(not led.value()) 
+           contador = 0
+        print(contador)     
+        time.sleep_ms(250)
